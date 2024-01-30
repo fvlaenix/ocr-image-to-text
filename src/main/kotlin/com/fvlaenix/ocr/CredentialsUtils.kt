c517ca65eb71a978777ddf8b4d3503f81aa784f5
fvlaenix <fvlaenixf@gmail.com>
2024-01-30 19:14:38 +0400

    Add credentials and refactor ocr utils

--- src/main/kotlin/com/fvlaenix/ocr/CredentialsUtils.kt
+++ src/main/kotlin/com/fvlaenix/ocr/CredentialsUtils.kt
@@ -4,7 +4,7 @@ import com.google.auth.oauth2.ServiceAccountCredentials
 import kotlin.io.path.Path
 import kotlin.io.path.inputStream
 
-val pathToCredentials = System.getProperty("ocr.credentials.path")?.let { Path(it) } ?: Path("credentials", "credentials.json")
+val pathToCredentials = System.getenv("ocr.credentials.path")?.let { Path(it) } ?: Path("credentials", "credentials.json")
 
 object CredentialsUtils {
   fun getCredentials(): ServiceAccountCredentials {
