3d0c2ba9378ee208b29dc46a482a1c0f709de90b
fvlaenix <fvlaenixf@gmail.com>
2024-01-31 14:04:39 +0400

    Change environment variable

--- src/main/kotlin/com/fvlaenix/ocr/CredentialsUtils.kt
+++ src/main/kotlin/com/fvlaenix/ocr/CredentialsUtils.kt
@@ -4,7 +4,7 @@ import com.google.auth.oauth2.ServiceAccountCredentials
 import kotlin.io.path.Path
 import kotlin.io.path.inputStream
 
-val pathToCredentials = System.getenv("ocr.credentials.path")?.let { Path(it) } ?: Path("credentials", "credentials.json")
+val pathToCredentials = System.getenv("OCR_CREDENTIALS_PATH")?.let { Path(it) } ?: Path("credentials", "credentials.json")
 
 object CredentialsUtils {
   fun getCredentials(): ServiceAccountCredentials {
